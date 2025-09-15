bad43ad004f49eb52697f65ea1b21aee1364fde7
rharding8 <ry287616@ucf.edu>
2025-09-15 17:02:14 -0400

    Updated Parallel Algorithm to Default Libraries. Already done before last commit, but I kept the Rayon version there as a backup.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::time::Instant;
-use rayon::ThreadPoolBuilder;
-use rayon::prelude::*;
+use std::thread;
 
 fn main() {
     let start = Instant::now();
@@ -44,35 +43,33 @@ pub fn seq_prime_finder(n: usize) -> Vec<usize> {
 }
 
 pub fn par_prime_finder(n: usize, threads: usize) -> Vec<usize> {
-    ThreadPoolBuilder::new().num_threads(threads).build_global().unwrap();
     let nsq: usize = n.isqrt();
     
-    let mut base_sieve = vec![true; nsq + 1];
+    let mut sieve = vec![true; nsq + 1];
+    sieve[0] = false;
+    sieve[1] = false;
     
     for i in 2..=nsq {
-        if base_sieve[i] {
+        if sieve[i] {
             let mut j = i.pow(2);
             while j <= nsq {
-                base_sieve[j] = false;
+                sieve[j] = false;
                 j = j + i;
             }
         }
     }
 
-    let base_primes: Vec<usize> = (2..=nsq).filter(|&i| base_sieve[i]).collect();
+    let base_primes: Vec<usize> = (2..=nsq).filter(|&i| sieve[i]).collect();
 
-    let mut par_sieve = vec![true; n + 1];
-    par_sieve[0] = false;
-    par_sieve[1] = false;
+    let chunk_size = (n + 1 - nsq) / threads + 1;
+    let mut handles = Vec::new();
 
-    let chunk_size = (n + 1) / threads + 1;
-    par_sieve
-        .par_chunks_mut(chunk_size)
-        .enumerate()
-        .for_each(|(i, chunk)| {
-            let start = i * chunk_size;
-
-            for &p in &base_primes {
+    for i in 0..threads {
+        let base = base_primes.clone();
+        let start = nsq + 1 + (i * chunk_size);
+        let hand = thread::spawn(move || {
+            let mut chunk = vec![true; chunk_size];
+            for &p in &base {
                 let mut multiple = if start <= p {
                     p * p
                 } else {
@@ -80,16 +77,25 @@ pub fn par_prime_finder(n: usize, threads: usize) -> Vec<usize> {
                     r * p
                 };
 
-                while multiple < start + chunk.len() && multiple <= n {
-                    if multiple >= start {
+                while multiple < start + chunk.len() {
+                    if multiple >= start && multiple <= n {
                         chunk[multiple - start] = false;
                     }
                     multiple += p;
                 }
             }
+            chunk
         });
+        handles.push(hand);
+    }
 
-    par_sieve
+    for hand in handles {
+        let mut next_sieve = hand.join().unwrap();
+        sieve.append(&mut next_sieve);
+    }
+    sieve.truncate(n + 1);
+
+    sieve
         .iter()
         .enumerate()
         .filter_map(|(i, &prime)| if prime { Some(i) } else { None })
