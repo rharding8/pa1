660b4b3bb9fa7d1ec9a335229954c230e1476ae6
rharding8 <ry287616@ucf.edu>
2025-09-22 00:03:40 -0400

    Added Comments and Readme. Renamed description pdf

---
+++ readme.md
@@ -0,0 +1,25 @@
+## Parallel Prime Finder
+# Ryan Harding, Due 9/24, COP6616
+
+## Compilation and Execution
+# RECOMMENDED: Cargo
+As I used Cargo as my environment for this project, executing with Cargo is recommended. To do so, run:
+    cargo run
+    OR
+    cargo run --release
+From the root of the project. The latter simply builds the project in target/release instead of target/debug, but both work.
+To compile without running, run:
+    cargo build
+    OR
+    cargo build --release
+
+# Without Cargo
+As the program uses all standard Rust libraries, it will compile and run without cargo, just using the "main.rs" file. To do so, run:
+    rustc main.rs
+    ./main
+The first command compiles, the second executes. Doing so outputs the results in the directory with "main.rs", not the project root.
+
+## Other Files
+"sequential.txt" is an example output produced using a sequential sieve.
+"pa1description.pdf" is the original assignment pdf from Webcourses.
+"pa1analysis.pdf" is the analysis document for the assignment.
--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,24 @@ use std::io::Result;
 use std::io::Error;
 
 fn main() -> Result<()> {
+    // Create Output File
     let mut file = File::create("output.txt")?;
+
+    // Run Prime Algorithm and Measure Execution Time
     let start = Instant::now();
-
     let par_primes = par_prime_finder(1e8 as usize, 8)?;
-
     let end = start.elapsed();
 
+    // Output Results
     write!(file, "Execution Time: {} seconds\t", end.as_secs())?;
     write!(file, "{} Primes Found\t", par_primes.len())?;
     writeln!(file, "Sum of Primes: {}", par_primes.iter().sum::<usize>())?;
-
     writeln!(file, "Top 10 Largest Primes:")?;
     for i in (1..=10).rev() {
         writeln!(file, "{}.\t {}", i, par_primes[par_primes.len() - i])?;
     }
 
+    // Output Error-Handling Result
     Ok(())
 }
 
@@ -52,10 +54,12 @@ pub fn seq_prime_finder(n: usize) -> Result<Vec<usize>> {
 pub fn par_prime_finder(n: usize, threads: usize) -> Result<Vec<usize>> {
     let nsq: usize = n.isqrt();
     
+    // Create Sieve Up To sqrt(N)
     let mut sieve = vec![true; nsq + 1];
     sieve[0] = false;
     sieve[1] = false;
     
+    // Execute Sequential Sieve Up To sqrt(N) For Base Primes
     for i in 2..=nsq {
         if sieve[i] {
             let mut j = i.pow(2);
@@ -65,12 +69,14 @@ pub fn par_prime_finder(n: usize, threads: usize) -> Result<Vec<usize>> {
             }
         }
     }
-
     let base_primes: Vec<usize> = (2..=nsq).filter(|&i| sieve[i]).collect();
-
+    
+    // Set Size of Chunks According to Thread Count (8)
     let chunk_size = (n + 1 - nsq) / threads + 1;
+    
+    // Run Parallel Sieve by Spawning One Thread Per Chunk to Mark Multiples
+    // of Base Primes
     let mut handles = Vec::new();
-
     for i in 0..threads {
         let base = base_primes.clone();
         let start = nsq + 1 + (i * chunk_size);
@@ -96,14 +102,20 @@ pub fn par_prime_finder(n: usize, threads: usize) -> Result<Vec<usize>> {
         handles.push(hand);
     }
 
+    // Join Threads and Append All Chunk Sieves onto the Base Sieve
     for hand in handles {
         let mut next_sieve = hand.join().map_err(|_| {
             Error::new(std::io::ErrorKind::Other, "thread panicked!")
         })?;
         sieve.append(&mut next_sieve);
     }
+
+    // LIMIT: The final chunk may break the bounds of N, leaving a
+    // few false-positive primes over N. Truncating the final, complete
+    // sieve to the Nth index (N+1 Size) removes these if they occur.
     sieve.truncate(n + 1);
 
+    // Collect and Return All Primes, Wrapped in a Result for Error-Handling
     Ok(sieve
         .iter()
         .enumerate()
